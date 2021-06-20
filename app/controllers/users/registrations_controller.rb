584363899498eb873643764786e31ca81536fe49
yuma0313 <y.yuma0313@gmail.com>
2021-06-20 12:40:19 +0000

    registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,12 +59,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-  
-  password = Devise.friendly_token.first(7)
-    if session[:provider].present? && session[:uid].present?
-      @user = User.create(name:session[:name], email: session[:email], password: "password", password_confirmation: "password")
-      sns = SnsCredential.create(user_id: @user.id,uid: session[:uid], provider: session[:provider])
-    else
-      @user = User.create(name:session[:name], email: session[:email], password: session[:password], password_confirmation: session[:password_confirmation])
-    end
+
+  # password = Devise.friendly_token.first(7)
+  #   if session[:provider].present? && session[:uid].present?
+  #     @user = User.create(name:session[:name], email: session[:email], password: "password", password_confirmation: "password")
+  #     sns = SnsCredential.create(user_id: @user.id,uid: session[:uid], provider: session[:provider])
+  #   else
+  #     @user = User.create(name:session[:name], email: session[:email], password: session[:password], password_confirmation: session[:password_confirmation])
+  #   end
 end
