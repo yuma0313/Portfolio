6551aac738d65f52a7968323c3b5e748734431c5
yuma0313 <y.yuma0313@gmail.com>
2021-07-11 03:46:17 +0000

    about

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,12 +3,13 @@ class PostImage < ApplicationRecord
   attachment :image
 
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user # いいねランキング
+  has_many :bookmarks, dependent: :destroy
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  has_many :favorited_users, through: :favorites, source: :user # いいねランキング
-
-  has_many :bookmarks, dependent: :destroy
+  
   def bookmarked_by?(user)
     bookmarks.where(user_id: user.id).exists?
   end
