63d39ba65e2d2753096a49b6f9bb775fb8a6ad00
yuma0313 <y.yuma0313@gmail.com>
2021-07-19 10:49:38 +0000

    a

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @post_images = PostImage.includes(:favorited_users).limit(4).sort do |a, b|
+    @post_images = PostImage.includes(:favorited_users).limit(4).sort do |a, b| #いいねの多い4つを表示
       b.favorited_users.size <=> a.favorited_users.size
     end
     @posts = PostImage.all
